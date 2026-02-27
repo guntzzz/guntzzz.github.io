94d5103829189e58ae404ed4a2eefe1a5b5c1b30
guntzzz <39553187+guntzzz@users.noreply.github.com>
2026-02-27 09:04:16 +0100

    Delete simple_physics_calculators directory

--- simple_physics_calculators/SPC.md
+++
@@ -1,3 +0,0 @@
-layout: page
-title: "Simple Physics Calculators"
-permalink: https://guntzzz.github.io/simple_physics_calculators
--- simple_physics_calculators/spc_header.php
+++
@@ -1,8 +0,0 @@
-<div class="header" id="pre-header">
-  <span class="right">simone@spc.it</span>
-  <span class="left">Pirri</span>
-</div>
-<div class="header">
-  <h1>Simple Physics Calculators</h1>
-  <p>For miscellaneous curiosities</p>
-</div>
