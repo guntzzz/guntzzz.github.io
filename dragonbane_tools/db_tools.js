51c11a664bdad79cba10d4e70204e52058be823f
guntzzz <simone.pilleri@gmail.com>
2026-03-05 11:36:37 +0100

    v1.1

--- dragonbane_tools/db_tools.js
+++ dragonbane_tools/db_tools.js
@@ -3,8 +3,26 @@ function dbInitiative() {
 	let data = document.getElementById("combPart").value;
 	data = data.split("\n");
 	cards = [1,2,3,4,5,6,7,8,9,10];
-	shuffleArray(cards);
 	var res = [];
+	var rem = [];
+	for (i = 0; i < data.length; i++) {
+		let kpTurn = [];
+		kpTurn = data[i].split(",");
+		console.log(kpTurn.length);
+		if (kpTurn.length == 2) {
+			res.push([parseInt(kpTurn[1]),kpTurn[0]]);
+			rem.push(data[i]);
+		}
+	}
+	console.log(rem);
+	for (i = 0; i < rem.length; i++) {
+		cards.splice(cards.indexOf(parseInt(rem[i].split(",")[1])),1);
+		console.log(rem[i].split(",")[1]);
+		data.splice(data.indexOf(rem[i]),1);
+	}
+	console.log(cards);
+	console.log(data);
+	shuffleArray(cards);
 	for (let i = 0; i < data.length; i++) {
 		res.push([cards[i],data[i]]);
 	}
