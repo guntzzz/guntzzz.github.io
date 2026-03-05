29c764503fd6a765fca2832ea1c4052b3184ce6c
guntzzz <simone.pilleri@gmail.com>
2026-03-05 22:03:15 +0100

    turn swap v1.2

--- dragonbane_tools/db_tools.js
+++ dragonbane_tools/db_tools.js
@@ -38,8 +38,9 @@ function dbInitiative() {
 		if (flag === 0) break;
 	}
 	for (let i = 0; i < res.length; i++) {
-		document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + `${res[i][0]} ${res[i][1]}<br>`;
+		document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + `<input type="checkbox" id="${i}"> ${res[i][0]} ${res[i][1]}<br>`;
 	}
+	document.getElementById("swapB").innerHTML = "<button type=\"button\" id=\"swap\"i onclick=\"swapTurns()\":w>Swap</button>";
 	console.log(res);
 }
 
@@ -48,4 +49,54 @@ function shuffleArray(array) {
 		const j = Math.floor(Math.random() * (i + 1));
 		[array[i], array[j]] = [array[j], array[i]];
 	}
+}
+
+function swapTurns() {
+	document.getElementById("swapB").innerHTML = "<button type=\"button\" id=\"swap\"i onclick=\"swapTurns()\":w>Swap</button>"; 
+	let data = document.getElementById("results").innerText;
+	data = data.split("\n");
+	var swap = [];
+	for (let i = 0; i < data.length-1; i++) {
+		data[i] = data[i].split(" ");
+		if (document.getElementById(`${i}`).checked == true) {
+			data[i].push(i)
+			swap.push(data[i]);
+		}
+	console.log(swap);
+	}
+	console.log(data);
+	if (swap.length != 2) {
+		error();
+	} else {
+		for (let i = 0; i < swap.length; i++) {
+			if (swap[i].length > 4) {
+				error();
+				break;
+			} else if (i == swap.length - 1) { 
+				[data[swap[0][3]][2],data[swap[1][3]][2]] = [data[swap[1][3]][2],data[swap[0][3]][2]];
+				data[swap[0][3]].splice(3,1,"*");
+				data[swap[1][3]].splice(3,1,"*");
+				console.log(swap[0][4]);
+				console.log(swap[1][4]);
+				break;
+			}
+		}
+	}
+	document.getElementById("results").innerHTML = ""; 
+	for (let i = 0; i < data.length - 1; i++) {
+		console.log(data[i]);
+		if (data[i].length == 3 || data[i][3] != "*") {
+			document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + `<input type="checkbox" id="${i}"> ${data[i][1]} ${data[i][2]}<br>`;
+		} else {
+			document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + `<input type="checkbox" id="${i}"> ${data[i][1]} ${data[i][2]} ${data[i][3]}<br>`;
+		}
+	}
+	console.log(data);
+}
+
+function error() {
+	r = Math.floor(Math.random() * 256);
+	g = Math.floor(Math.random() * 256);
+	b = Math.floor(Math.random() * 256);
+	document.getElementById("swapB").innerHTML = "<button type=\"button\" id=\"swap\"i onclick=\"swapTurns()\":w>Swap</button>" + ` <span style="color:rgb(${r},${g},${b})"> Invalid Swap</span>`;
 }
